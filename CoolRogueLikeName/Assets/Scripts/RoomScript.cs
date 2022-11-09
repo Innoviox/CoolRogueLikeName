f72c2e968d9a6cef999f074b88c1e4fe7a833e86
Simon Chervenak <sarras305@gmail.com>
2022-11-08 20:49:42 -0500

    Revert "Merge branch 'main' into animate-camera"
    This reverts commit 24108fa597cc8b5a2c5f9ddd73756b72bae4431e, reversing
    changes made to 41099c4c40d3a8d8fe0d5845dd6d984fe897fc15.

--- CoolRogueLikeName/Assets/Scripts/RoomScript.cs
+++ CoolRogueLikeName/Assets/Scripts/RoomScript.cs
@@ -12,7 +12,7 @@ public class RoomScript : MonoBehaviour
     public bool generateEnemies = true; // todo make private with getter/setter
     public Camera camera;
     private int nEnemies;
-    private List<Transform> doors;
+    private List<DoorScript> doors;
 
     private Transform room;
     private Bounds bounds;
@@ -25,7 +25,7 @@ public class RoomScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        doors = new List<Transform>();
+        doors = new List<DoorScript>();
 
         Transform door;
         int n = 1;
@@ -59,7 +59,6 @@ public class RoomScript : MonoBehaviour
         // fit room bounds to children
         foreach (Collider collider in GetComponentsInChildren<Collider>())
         {
-            Debug.Log($"RoomScript: {collider.name} {collider.bounds}");
             bounds.Encapsulate(collider.bounds);
         }
 
@@ -89,8 +88,7 @@ public class RoomScript : MonoBehaviour
             // open all doors
             foreach (var door in doors)
             {
-                // todo make list of DoorScripts so this is faster
-                door.gameObject.SendMessage("Unlock");
+                door.Unlock();
             }
 
             if (entryPoint != null)
@@ -110,7 +108,7 @@ public class RoomScript : MonoBehaviour
         foreach (var door in doors)
         {
             // todo make this better
-            var newRoom = door.gameObject.GetComponent<DoorScript>().GenerateRoom(player);
+            var newRoom = door.GenerateRoom(player);
             newRoom.gameObject.GetComponent<RoomScript>().pregenerateDepth = pregenerateDepth - 1;
             newRoom.gameObject.GetComponent<RoomScript>().generateEnemies = false;
             newRoom.gameObject.GetComponent<RoomScript>().camera = camera;
@@ -168,8 +166,9 @@ public class RoomScript : MonoBehaviour
         return bounds.Contains(player.position + playerBounds.min) && bounds.Contains(player.position + playerBounds.max);
     }
 
-    public void AddDoor(Transform door)
+    public void AddDoor(DoorScript door)
     {
+        door.player = player;
         doors.Add(door);
     }
 

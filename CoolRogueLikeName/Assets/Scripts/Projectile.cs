06c9dc4ba843eb9704b6295cd258bf4dfaa67421
jbaq <66805959+jbaq@users.noreply.github.com>
2022-12-05 12:32:32 -0500

    Update Projectile.cs

--- CoolRogueLikeName/Assets/Scripts/Projectile.cs
+++ CoolRogueLikeName/Assets/Scripts/Projectile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float Damage;
-
+    public int collisionCount;
     // Projectile ends its life when colliding with a wall
     // or an enemy. 
     private void OnCollisionEnter(Collision collision)
@@ -13,9 +13,16 @@ public class Projectile : MonoBehaviour
         // Don't destroy myself if I collide with other bullets
         if (collision.transform.gameObject.name != "Bullet(Clone)")
         {
+            
             // Destroy myself :(
             Destroy(gameObject);
         }
 
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.gameObject.name.Contains("Wall") || other.transform.gameObject.name.Contains("Door") || other.transform.gameObject.name.Contains("Floor")) {
+            Destroy(gameObject);
+        }
     }
 }

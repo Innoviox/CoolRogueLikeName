a9f06bc40909e3fcde7494e90226cdf5ebe86b65
Simon Chervenak <sarras305@gmail.com>
2022-12-04 21:31:49 -0500

    fix coordinate off-by-ones

--- CoolRogueLikeName/Assets/Scripts/Dungeons/RoomGenerator.cs
+++ CoolRogueLikeName/Assets/Scripts/Dungeons/RoomGenerator.cs
@@ -11,26 +11,27 @@ public class RoomGenerator : MonoBehaviour
 
     public void GenerateRoom(Transform roomRootTransform, Vector3 center, int x, int z)
     {
+        var delta = 1.0f;
         // GameObject roomRoot = new GameObject("Room " + (int)center.x + " " + (int)center.y + " " + (int)center.z);
         // Transform roomRootTransform = roomRoot.GetComponent<Transform>();
-        for (float i = - x + 0.5f; i < x; i++)
+        for (float i = - x + 1.5f; i < x - 1; i++)
         {
-            Instantiate(side, new Vector3(center.x + i, center.y, center.z - z - 0.5f), Quaternion.identity, roomRootTransform);
-            Instantiate(side, new Vector3(center.x + i, center.y, center.z + z + 0.5f), Quaternion.Euler(0, 180, 0), roomRootTransform);
-            for (float j = - z + 0.5f; j < z; j++)
+            Instantiate(side, new Vector3(center.x + i, center.y, center.z - z - 0.5f + 1.5f), Quaternion.identity, roomRootTransform);
+            Instantiate(side, new Vector3(center.x + i, center.y, center.z + z + 0.5f - 1.5f), Quaternion.Euler(0, 180, 0), roomRootTransform);
+            for (float j = - z + 1.5f; j < z - 1; j++)
             {
                 Instantiate(floor, new Vector3(center.x + i, center.y, center.z + j), Quaternion.identity, roomRootTransform);
             }
         }
-        for (float i = - z + 0.5f; i < z; i++)
+        for (float i = - z + 1.5f; i < z - 1; i++)
         {
-            Instantiate(side, new Vector3(center.x - x - 0.5f, center.y, center.z + i), Quaternion.Euler(0, 90, 0), roomRootTransform);
-            Instantiate(side, new Vector3(center.x + x + 0.5f, center.y, center.z + i), Quaternion.Euler(0, 270, 0), roomRootTransform);
+            Instantiate(side, new Vector3(center.x - x - 0.5f + 1.5f, center.y, center.z + i), Quaternion.Euler(0, 90, 0), roomRootTransform);
+            Instantiate(side, new Vector3(center.x + x + 0.5f - 1.5f, center.y, center.z + i), Quaternion.Euler(0, 270, 0), roomRootTransform);
         }
-        Instantiate(corner, new Vector3(center.x + x + 0.5f, center.y, center.z + z + 0.5f), Quaternion.Euler(0, 270, 0), roomRootTransform);
-        Instantiate(corner, new Vector3(center.x - x - 0.5f, center.y, center.z + z + 0.5f), Quaternion.Euler(0, 180, 0), roomRootTransform);
-        Instantiate(corner, new Vector3(center.x - x - 0.5f, center.y, center.z - z - 0.5f), Quaternion.Euler(0, 90, 0), roomRootTransform);
-        Instantiate(corner, new Vector3(center.x + x + 0.5f, center.y, center.z - z - 0.5f), Quaternion.Euler(0, 0, 0), roomRootTransform);
+        Instantiate(corner, new Vector3(center.x + x + 0.5f - 1.5f, center.y, center.z + z + 0.5f - 1.5f), Quaternion.Euler(0, 270, 0), roomRootTransform);
+        Instantiate(corner, new Vector3(center.x - x - 0.5f + 1.5f, center.y, center.z + z + 0.5f - 1.5f), Quaternion.Euler(0, 180, 0), roomRootTransform);
+        Instantiate(corner, new Vector3(center.x - x - 0.5f + 1.5f, center.y, center.z - z - 0.5f + 1.5f), Quaternion.Euler(0, 90, 0), roomRootTransform);
+        Instantiate(corner, new Vector3(center.x + x + 0.5f - 1.5f, center.y, center.z - z - 0.5f + 1.5f), Quaternion.Euler(0, 0, 0), roomRootTransform);
 
         BoxCollider floorCollider = roomRootTransform.gameObject.AddComponent<BoxCollider>();
         floorCollider.size = new Vector3(2 * x + 2, 1, 2 * z + 2);

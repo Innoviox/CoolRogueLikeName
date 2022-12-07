4c260b1340b8dd46dd75a5191256d046d9b06a75
jbaq <66805959+jbaq@users.noreply.github.com>
2022-12-06 21:29:30 -0500

    SetCenter needed transform argument

--- CoolRogueLikeName/Assets/Scripts/Enemies/EnemyCreator.cs
+++ CoolRogueLikeName/Assets/Scripts/Enemies/EnemyCreator.cs
@@ -25,7 +25,7 @@ public class EnemyCreator : MonoBehaviour
         var boss = Instantiate(bossPrefab, position, new Quaternion());
         boss.parent = transform;
         boss.BroadcastMessage("SetPlayer", player);
-        boss.BroadcastMessage("SetCenter", Instantiate(new GameObject(), position, new Quaternion()));
+        boss.BroadcastMessage("SetCenter", Instantiate(new GameObject(), position, new Quaternion()).transform);
 
         return boss;
     }

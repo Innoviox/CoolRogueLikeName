b38021588d5065264a26212dbc044b7edf9bd4eb
Simon Chervenak <sarras305@gmail.com>
2022-12-06 19:35:41 -0500

    okay

--- CoolRogueLikeName/Assets/Scripts/MusicPlayer.cs
+++ CoolRogueLikeName/Assets/Scripts/MusicPlayer.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
+    public AudioClip[] A;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void PlayNote()
+    {
+
+    }
+
+    void PlayChord()
+    {
+
+    }
+
 }

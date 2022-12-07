c67b9e7e7ced8782d3264574ef5fa19ff75cdceb
Simon Chervenak <sarras305@gmail.com>
2022-12-06 19:59:03 -0500

    play chord

--- CoolRogueLikeName/Assets/Scripts/MusicPlayer.cs
+++ CoolRogueLikeName/Assets/Scripts/MusicPlayer.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-    public AudioClip[] A;
+    private List<AudioSource> sources;
+
+    public AudioClip[] A; // choral music notes in inspector don't ask why
+    public AudioClip[] pianoNotes; // todo
+    private AudioClip[] choralNotes;
+    private string[] noteNames = { "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" };
 
     // Start is called before the first frame update
     void Start()
     {
+        choralNotes = A;
 
+        sources = new List<AudioSource>();
+        for (int i = 0; i < 3; i++)
+        {
+            sources.Add(gameObject.AddComponent<AudioSource>());
+        }
+
+        PlayChord("C", 4, true);
     }
 
     // Update is called once per frame
@@ -18,14 +32,27 @@ public class MusicPlayer : MonoBehaviour
 
     }
 
-    void PlayNote()
+    int NoteIndex(string note, int octave)
     {
-
+        return Array.IndexOf(noteNames, note) + (octave - 1) * 12;
     }
 
-    void PlayChord()
+    void PlayNote(AudioSource source, int idx, bool choral)
     {
+        AudioClip clip = choral ? choralNotes[idx] : pianoNotes[idx];
 
+        source.clip = clip;
+        source.Play();
     }
 
+    void PlayChord(string startNote, int octave, bool major)
+    {
+        int idx = NoteIndex(startNote, octave);
+        int[] chord = major ? new int[] { 0, 4, 7 } : new int[] { 0, 3, 7 };
+
+        for (int i = 0; i < chord.Length; i++)
+        {
+            PlayNote(sources[i], idx + chord[i], true);
+        }
+    }
 }

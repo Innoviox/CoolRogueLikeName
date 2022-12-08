40031d53edce2d779e27a9e883b23d057874b890
Evan Guenterberg <evanguenterberg@gmail.com>
2022-12-07 20:26:24 -0500

    First step in weapons refactor

--- CoolRogueLikeName/Assets/Scripts/Weapons/BaseWeapon.cs
+++ CoolRogueLikeName/Assets/Scripts/Weapons/BaseWeapon.cs
@@ -6,11 +6,13 @@ public class BaseWeapon : MonoBehaviour
 {
     public Transform spawnPoint;   // Select this through the inspector
     public GameObject projectile;  // Selected Bullet prefab through inspector
-    public float projectileSpeed = 5;
+    public float baseProjectileSpeed = 5;
+    public float baseCooldown = 0.25f;
     public int baseDamage = 1;
+    public PowerupManager stats;
+    public string firebutton;
 
-
-    // Update is called once per frame
+    /*// Update is called once per frame
     void Update()
     {
         //if (Input.GetButton("Fire1"))         // Uncomment me for scatter shot 
@@ -25,5 +27,43 @@ public class BaseWeapon : MonoBehaviour
             // Set its velocity to go forward by projectileSpeed
             bullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * projectileSpeed;
         }
+    }*/
+
+    void Start()
+    {
+        EnableWeapon();
     }
+
+    public void EnableWeapon()
+    {
+        StartCoroutine(WeaponCoroutine());
+    }
+    public void DisableWeapon()
+    {
+        StopCoroutine(WeaponCoroutine());
+    }
+
+    IEnumerator WeaponCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitUntil(() => Input.GetButtonDown(firebutton));
+            DoWeaponAction(baseDamage * stats.playerDamageFactor, baseProjectileSpeed * stats.bulletSpeedFactor);
+            yield return new WaitForSeconds(baseCooldown * stats.playerReloadSpeedFactor);
+        }
+    }
+
+    // Don't do any powerup scaling, it has already been done
+    public virtual void DoWeaponAction(float damage, float projectileSpeed)
+    {
+        // Create a bullet from the prefab 
+        GameObject bullet = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
+        GetComponent<AudioSource>().Play();
+        // Set projectiles damage
+        bullet.GetComponent<Projectile>().Damage = baseDamage;
+
+        // Set its velocity to go forward by projectileSpeed
+        bullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * projectileSpeed;
+    }
+
 }
